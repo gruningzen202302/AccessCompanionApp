ff3721819d49c7729618b931671e048f7b59ff22
vir-tkp-w11 <qimono76live@outlook.com>
2023-12-04 21:09:35 -0300

    ci:first commit for project creation

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AccessCompanionApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
